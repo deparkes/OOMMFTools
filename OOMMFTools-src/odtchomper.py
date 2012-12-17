319cb96fcbdb9d89a7ac1b2afc9675fb810502f2
Duncan Parkes <ppxdep@nottingham.ac.uk>
2012-12-17 23:14:07 +0000

    Moved import statements outside of def main
    I had thought I needed to have them inside def main() to avoid some
    errors, but apparently not.

--- OOMMFTools-src/odtchomper.py
+++ OOMMFTools-src/odtchomper.py
@@ -18,11 +18,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 """
-
+import chomper
+import os
+import sys
 def main():
-    import chomper
-    import os
-    import sys
+
     args = chomper.get_args()
     if not args.batch_path:
         chomper.do_chomp(args)
