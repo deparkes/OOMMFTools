5c906e4de819c56054ebdea809f9115504ac5177
Duncan Parkes <ppxdep@nottingham.ac.uk>
2012-12-18 04:50:42 +0000

    Add output filetype selector

--- OOMMFTools-src/decoder.py
+++ OOMMFTools-src/decoder.py
@@ -6,14 +6,21 @@ import os
 def do_decode(args):
     in_file = args.in_file
     out_file = args.out_file
+    file_type = args.file_type
+
     if not out_file:
-        out_file = in_file + '.mat'
-##        out_file = in_file + '.pkl' # For numpy output
-        
-    array, headers, extra = oommfdecode.unpackFile(in_file)
-    oommfdecode.matlabifyArray(array, headers, extra, out_file)
-    #oommfdecode.pickleArray(array, headers, extra, 'out.mat')
+        out_file = in_file
     
+    array, headers, extra = oommfdecode.unpackFile(in_file)
+    
+    if file_type == 'matlab':
+        oommfdecode.matlabifyArray(array, headers, extra, out_file + '.mat')
+    elif file_type == 'pickle':
+        oommfdecode.pickleArray(array, headers, extra, out_file + '.pkl')
+    else:
+        oommfdecode.pickleArray(array, headers, extra, out_file + '.pkl')
+        oommfdecode.matlabifyArray(array, headers, extra, out_file + '.mat')
+            
 def batch_decoder(args):
     batch_path = args.batch_path
     for r,d,f in os.walk(batch_path):
@@ -32,6 +39,7 @@ def get_args(args = False):
     parser.add_argument("in_file", type=str, help="specify oommf vector file to load", nargs='?')
     parser.add_argument('-p', action="store_true", dest='show_headers', default=False, help='show headers')
     parser.add_argument('-o', dest='out_file', help='Set output file')
+    parser.add_argument('-t', dest='file_type', help='specify output file types', default='matlab',nargs='?', choices=['matlab', 'pickle', 'both'])
     parser.add_argument('out_file',nargs='?', help='Set output file', type=str)
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
     if not args:
