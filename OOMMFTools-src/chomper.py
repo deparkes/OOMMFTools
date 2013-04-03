3e2d8812ae9aebe291514a1744efdf2b512f761f
Duncan Parkes <ppxdep@nottingham.ac.uk>
2013-04-03 17:44:49 +0100

    Remove batch arg_parser reference.

--- OOMMFTools-src/chomper.py
+++ OOMMFTools-src/chomper.py
@@ -85,10 +85,8 @@ def do_chomp(args):
 			fields = odtfile.getNames()
 	write(out_file, odtfile, delim, fields)
      
-def get_args(args = False):
-    
+def get_args(args = False):    
     parser = argparse.ArgumentParser(description='Command line interface for odtchomp')
-    parser.add_argument('-b', type=str, action='store', dest='batch_path', help='Specify batch path')
     parser.add_argument("in_file", type=str, help="specify .odt file to load", nargs='?')
     parser.add_argument('-f', type=str, action='store', dest='fields', help='File with required headers')    
     parser.add_argument('-p', action="store_true", dest='show_headers', default=False, help='show headers')
