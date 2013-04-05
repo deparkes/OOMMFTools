18598fb40a11e69ee227e76b2e637758c2d13cb5
Duncan Parkes <ppxdep@nottingham.ac.uk>
2013-04-05 10:03:11 +0100

    Put decoder.py into omf2mat.py

--- OOMMFTools-src/omf2mat.py
+++ OOMMFTools-src/omf2mat.py
@@ -32,7 +32,6 @@ optional arguments:
 
 Requirements:
 - Python 2.7
-- decoder.py
 - oommfdecode.py
 
 TODO:
@@ -41,6 +40,8 @@ TODO:
 - Say output filename in screen output.
 
 Copyright (C) 2012  Duncan Parkes
+ppxdep@nottingham.ac.uk
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
@@ -57,11 +58,57 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 """
 
-import sys
-import decoder
+import oommfdecode
+import argparse
 import os
+import sys
+
+def do_decode(args):
+    in_file = args.in_file
+    out_file = args.out_file
+    file_type = args.file_type
+
+    if not out_file:
+        out_file = in_file
+    
+    array, headers, extra = oommfdecode.unpackFile(in_file)
+    
+    if file_type == 'matlab':
+        print 'Generating matlab compatible output'
+        oommfdecode.matlabifyArray(array, headers, extra, out_file + '.mat')
+    elif file_type == 'pickle':
+        print 'Generating numpy compatible output'
+        oommfdecode.pickleArray(array, headers, extra, out_file + '.pkl')
+    else:
+        print 'Generating output compatible with numpy and matlab'
+        oommfdecode.pickleArray(array, headers, extra, out_file + '.pkl')
+        oommfdecode.matlabifyArray(array, headers, extra, out_file + '.mat')
+            
+def batch_decoder(args):
+    batch_path = args.batch_path
+    for r,d,f in os.walk(batch_path):
+        for files in f:
+            if files.endswith(('.omf', '.ohf', '.oef', '.ovf')):
+                for avf in files.split():
+                    args.in_file = os.path.join(r,avf)
+                    do_decode(args)
+
+def get_args(args = False):
+    
+    parser = argparse.ArgumentParser(description='Command line interface for oommfdecode')
+    parser.add_argument('-b', type=str, action='store', dest='batch_path', help='Specify batch path')
+    parser.add_argument("in_file", type=str, help="specify oommf vector file to load", nargs='?')
+    parser.add_argument('-o', dest='out_file', help='Set output file')
+    parser.add_argument('-t', dest='file_type', help='specify output file types', default='matlab',nargs='?', choices=['matlab', 'pickle', 'both'])
+    parser.add_argument('out_file',nargs='?', help='Set output file', type=str)
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+    if not args:
+        return parser.parse_args()
+    else:
+        return parser.parse_args(args)
+
 def main():
-    args = decoder.get_args()
+    args = get_args()
     
     if not args.batch_path:
         # The batch processor only finds compatible files. This makes sure the user
@@ -69,10 +116,10 @@ def main():
         if not args.in_file.lower().endswith(('.omf', '.ohf', '.oef', '.ovf')):
             print "Please enter valid OOMMF file: ['.omf', '.ohf', '.oef', '.ovf']"
             sys.exit(1)
-        decoder.do_decode(args)
+        do_decode(args)
     else:
         start_dir = os.path.dirname(sys.argv[0])
-        decoder.batch_decoder(args)
+        batch_decoder(args)
         os.chdir(start_dir)
 if __name__=="__main__":
     main()
