cda3ad0d085f360a85df078d98bebba5e4d85af9
Duncan Parkes <deparkes@gmail.com>
2017-05-23 21:32:27 +0100

    Add passing unit test for filterOnExtension
    with unittest framework

--- tests/test_fnameutil.py
+++ tests/test_fnameutil.py
@@ -3,8 +3,13 @@ TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
 sys.path.insert(0, PROJECT_DIR)
 
+import unittest
 from OOMMFTools import fnameutil
-
-def test_filterOnExtensions():
-    test = fnameutil.filterOnExtensions(["one, two, three"],["four, five, six"])
-    assert test == 5
+class Test_fnameutil(unittest.TestCase):
+    def test_filterOnExtensions(self):
+        '''
+        Test filteronExtensions functionality.
+        '''
+        test_output = fnameutil.filterOnExtensions(["ohf", "omf"],["test_omf.omf", "test_ohf.ohf", "test_txt.txt"])
+        print(test_output)
+        self.assertEqual(test_output, ["test_omf.omf", "test_ohf.ohf"])
