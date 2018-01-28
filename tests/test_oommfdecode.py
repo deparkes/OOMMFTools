28be0e7af1acdd26b4ef46394a52edea9635754f
Duncan Parkes <deparkes@gmail.com>
2018-01-28 16:55:44 +0000

    Add draft tests for binary and text files

--- tests/test_oommfdecode.py
+++ tests/test_oommfdecode.py
@@ -1,30 +1,39 @@
 import sys, os
 import StringIO
 import tempfile
+import numpy as np
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
 sys.path.insert(0, PROJECT_DIR)
 
 import unittest
 from oommftools import oommfdecode
-class Test_OOMMFDecode(unittest.TestCase):
-    def test_groupUnpack(self):
-        pass
-    
-    def test_unpackFile(self):
-        pass
+
+
+import StringIO
+from oommftools.fnameutil import filterOnExtensions
+
+
+class Test_oommfdecode(unittest.TestCase):
+    def setUp(self):
+        self.test_files_folder = 'testfiles'
+        self.vector_file_text = os.path.join(TEST_DIR, 
+                                        self.test_files_folder,
+                                        'sliding_field0.ovf')
+        print(self.vector_file_text)
+        self.vector_file_binary = os.path.join(TEST_DIR, 
+                                        self.test_files_folder,
+                                        'h2h_leftedge_40x4.ohf')
+
+    def test_unpackFile_text(self):
+        (targetarray, headers, extraCaptures) = oommfdecode.unpackFile(self.vector_file_text)
+        assertEqual(targetarray, '1')
         
-    def test_textDecode(self):
-        pass
+    def test_unpackFile_binary(self):
+        (targetarray, headers, extraCaptures) = oommfdecode.unpackFile(self.vector_file_binary)
+        self.assertEqual(targetarray, '1')
         
-    def test_binaryDecode(self):
-        pass
         
-    def test_pickleArray(self):
-        pass
-    
-    def test_matlabifyArray(self):
-        pass
         
-    def test_slowlyPainfullyMaximize(self):
-        pass
+        
+        
