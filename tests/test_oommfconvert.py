518cceb2c27da0f639884c2e03b119b06b613c29
Duncan Parkes <deparkes@gmail.com>
2018-01-28 07:26:01 +0000

    Create oommfconvert test file

---
+++ tests/test_oommfconvert.py
@@ -0,0 +1,50 @@
+import sys, os
+import StringIO
+import tempfile
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
+sys.path.insert(0, PROJECT_DIR)
+
+import unittest
+from oommftools import oommfconvert
+import wx
+#class Test_OOMMFConvert(unittest.TestCase):
+#    def setUp(self):
+#        pass
+#       
+#    
+#    def test_OnDropFilesGoodFiles(self):
+#        x = 1
+#        y = 1
+#        self.main.doNumpy.SetValue(True)
+#        filenames = ['test.omf','test.ohf']
+#        self.assertTrue(self.main.dt.OnDropFiles(x,y,filenames))
+#
+#    def test_OnDropFilesBadFile(self):
+#        x = 1
+#        y = 1
+#        filenames = ['test.ozf','test.ohf']
+#        self.main.doNumpy.SetValue(True)
+#        self.assertEqual(self.main.dt.OnDropFiles(x,y,filenames), None)
+#        
+#
+#    def test_groupUnpack(self):
+#        pass
+#        
+#    def test_unpackFile(self):
+#        pass
+#        
+#    def test_textDecode(self):
+#        pass
+#        
+#    def test_binaryDecode(self):
+#        pass
+#        
+#    def test_pickleArray(self):
+#        pass
+#        
+#    def test_matlabifyArray(self):
+#        pass
+#    
+#    def test_slowlyPainfullyMaximize(self):
+#        pass
