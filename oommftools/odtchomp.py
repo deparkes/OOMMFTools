6abbd5a2c7d9c817bfff8c7f10a757ddd19290ec
Duncan Parkes <deparkes@gmail.com>
2018-02-16 20:37:04 +0000

    Fix multiline imports

--- oommftools/odtchomp.py
+++ oommftools/odtchomp.py
@@ -19,7 +19,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 """
 
-import os, wx
+import os
+import wx
 import numpy as np
 from fnameutil import filterOnExtensions
 
