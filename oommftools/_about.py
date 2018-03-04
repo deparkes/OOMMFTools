74ce8442f4fd310cd1489f7df43179c7ebc94680
Duncan Parkes <deparkes@gmail.com>
2018-03-04 15:49:29 +0000

    Create _about.py for 'about' info'

---
+++ oommftools/_about.py
@@ -0,0 +1,24 @@
+import os.path
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__commit__",
+    "__author__", "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "OOMMFTools"
+__summary__ = ("OOMMFTools is a set of drag-and-drop GUI utilities ",
+    "to assist in OOMMF postprocessing, including image",
+    "conversion and vector file manipulation.")
+__uri__ = 'http://web.mit.edu/daigohji/projects/OOMMFTools/'
+
+__version__ = "2.0.0"
+
+__author__ = "Mark Mascaro and Contributors"
+__email__ = "donald@stufft.io"
+
+__license__ = ("OOMMFTools is free software; you can redistribute",
+            "it and/or modify it under the terms of the GNU ",
+            "General Public License as published by the Free ",
+            "Software Foundation, either version 2 of the License,"
+            "or (at your option) any later version.")
+__copyright__ = "2010 %s" % __author__
