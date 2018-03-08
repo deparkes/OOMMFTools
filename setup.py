66c51908dd845a35f791b1a2c24f2a60ba0b88d2
Duncan Parkes <deparkes@gmail.com>
2018-03-08 14:51:22 +0000

    Tidy setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup
 
+try:
+    import m2r
+    long_description = m2r.parse_from_file('README.md')
+except(IOError, ImportError):
+    print('Markdown not converted to rst')
+    long_description = open('README.md').read()
+
 about = {}
 with open("./oommftools/_about.py") as fp:
     exec(fp.read(), about)
-
-with open("README.md", 'r') as f:
-    long_description = f.read()
     
 setup(
    name=about['__title__'].lower(),
-   version=about['__version__'],
-   description=about['__summary__'],
+   version=about['__version__']+'dev4',
+   description=''.join(about['__summary__']),
    long_description=long_description,
    author=about['__author__'],
    author_email=about['__email__'],
+   url=about['__uri__'],
    packages=['oommftools'],  #same as name
    install_requires=['wxpython', 'scipy', 'numpy'], #external packages as dependencies
 )
