8bda125f9d9711c009ac74bbc53cd2fe5e44bc15
Duncan Parkes <deparkes@gmail.com>
2018-05-14 21:15:32 +0100

    Fix some pep8 issues

--- setup.py
+++ setup.py
@@ -14,29 +14,29 @@ with open("./oommftools/_about.py") as fp:
     exec(fp.read(), about)
     
 setup(
-   name=about['__title__'].lower(),
-   version=about['__version__'],
-   description=''.join(about['__summary__']),
-   long_description=long_description,
-   author=about['__author__'],
-   maintainer=about['__maintainer__'],
-   license='GPLv2',
-   author_email=about['__email__'],
-   url=about['__uri__'],
-   packages=['oommftools'],  #same as name
-   setup_requires=['pytest-runner'],
-   install_requires=['scipy', 'numpy', 'future', 'pytest'] + (
-        ["wxpython"] if not sys.platform.startswith("linux") else []
-        ), #external packages as dependencies
+    name=about['__title__'].lower(),
+    version=about['__version__'],
+    description=''.join(about['__summary__']),
+    long_description=long_description,
+    author=about['__author__'],
+    maintainer=about['__maintainer__'],
+    license='GPLv2',
+    author_email=about['__email__'],
+    url=about['__uri__'],
+    packages=['oommftools'],  # same as name
+    setup_requires=['pytest-runner'],
+    install_requires=['scipy', 'numpy', 'future', 'pytest'] + (
+            ["wxpython"] if not sys.platform.startswith("linux") else []
+            ),  # external packages as dependencies
     tests_require=['pytest'],
     classifiers=[
-    'Operating System :: MacOS :: MacOS X',
-    'Operating System :: Microsoft :: Windows',
-    'Operating System :: POSIX',
-    'Programming Language :: Python :: 2.7',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering :: Physics',
-    'Topic :: Scientific/Engineering :: Visualization',
-    'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
-    ],
+                 'Operating System :: MacOS :: MacOS X',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Programming Language :: Python :: 2.7',
+                 'Intended Audience :: Science/Research',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Topic :: Scientific/Engineering :: Visualization',
+                 'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+                ],
 )
