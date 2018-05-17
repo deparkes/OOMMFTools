caf1def921b996f40c2939ea296d366b956e284b
Duncan Parkes <deparkes@gmail.com>
2018-05-17 09:50:09 +0100

    Add getOOMMFPath tests

--- tests/test_oommfconvert.py
+++ tests/test_oommfconvert.py
@@ -8,7 +8,34 @@ PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
 sys.path.insert(0, PROJECT_DIR)
 
 import unittest
-from oommftools import oommfconvert
+from oommftools.core import oommfconvert as oommfconvert
+
+
+class Test_getOOMMFPath(unittest.TestCase):
+    def setUp(self):
+        with tempfile.NamedTemporaryFile(delete=False) as self.valid_file:
+            # Create dummy oommf.path file
+            self.valid_file.write(r'C:\oommf-1.2a5bis\oommf.tcl'.encode())
+            self.valid_file.flush()
+
+        with tempfile.NamedTemporaryFile(delete=False) as self.invalid_path:
+            # Create dummy oommf.path file
+            self.invalid_path.write(r'C:\oommf-1.2a5bis'.encode())
+            self.invalid_path.flush()
+
+    def test_valid_path(self):
+        path = oommfconvert.getOOMMFPath(self.valid_file.name)
+        self.assertEqual(path, r'C:\oommf-1.2a5bis\oommf.tcl')
+
+    def test_invalid_path(self):
+        path = oommfconvert.getOOMMFPath(self.invalid_path.name)
+        self.assertEqual(path, None)
+
+    def test_no_file_found(self):
+        path = oommfconvert.getOOMMFPath('bad/path')
+        self.assertEqual(path, None)
+
+
 class Test_OOMMFConvert(unittest.TestCase):
     def test_doImages(self):
         pass
